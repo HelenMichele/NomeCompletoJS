2f98976b74a8e81fc074d04a6bfe261ae1292995
José Helder <jose.silva2000@etec.sp.gov.br>
2023-05-02 16:53:42 -0300

    atividade helen

---
+++ index.js
@@ -0,0 +1,11 @@
+let nome = prompt ("Digite seu primeiro nome");
+
+let sobrenome = prompt ("Digite seu sobrenome");
+
+
+let nomeCompleto = `${nome} ${sobrenome}`;
+let nomeCatalogo = `${sobrenome.toUpperCase()} ${nome}`;
+
+
+alert (nomeCompleto);
+alert (nomeCatalogo);
